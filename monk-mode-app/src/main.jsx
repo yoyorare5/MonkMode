12383ba876fedb8d1181d9103b64da3cb070ac7a
yoyorare5 <58817428+yoyorare5@users.noreply.github.com>
2026-04-25 15:05:39 -0400

    Load iPhone bugfix styles last

--- monk-mode-app/src/main.jsx
+++ monk-mode-app/src/main.jsx
@@ -7,6 +7,7 @@ import './final-color-fix.css'
 import './command-page.css'
 import './command-tuning.css'
 import './screen-mockup.css'
+import './iphone-bugfix.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
