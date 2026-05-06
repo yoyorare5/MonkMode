60a2adbe3b15d6de0f2865db423e382c94e68df4
yoyorare5 <58817428+yoyorare5@users.noreply.github.com>
2026-05-06 18:35:21 -0400

    Wire app entry to Fasting Mode source

--- monk-mode-app/src/main.jsx
+++ monk-mode-app/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './FastingModeApp'
 import './index.css'
+import './routine-goals.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
