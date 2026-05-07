e5c30c9b09a97e935f06687445930e23b28c82e7
yoyorare5 <58817428+yoyorare5@users.noreply.github.com>
2026-05-07 13:01:52 -0400

    Import Goals polish styles

--- monk-mode-app/src/main.jsx
+++ monk-mode-app/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './FastingModeApp'
 import './index.css'
 import './routine-goals.css'
+import './goal-polish.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
