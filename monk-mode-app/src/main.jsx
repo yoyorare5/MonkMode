70716147d54a891aaf791bbd7b03a7e3eaf8cb81
yoyorare5 <58817428+yoyorare5@users.noreply.github.com>
2026-05-08 14:35:21 -0400

    Load goal modal mobile fixes

--- monk-mode-app/src/main.jsx
+++ monk-mode-app/src/main.jsx
@@ -4,6 +4,7 @@ import App from './FastingModeApp'
 import './index.css'
 import './routine-goals.css'
 import './goal-polish.css'
+import './goal-modal-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
